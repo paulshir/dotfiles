70d8cbc5bac2c06c3d1c3df10d89227ab0b16e48
Paul Shirley <paulshir@gmail.com>
2014-07-29 20:39:41 +0100

    Updating the powershell profile

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -9,42 +9,10 @@ Get-ChildItem "$dotfiles\*\*.ps1" | where { $_ -and ($exclude -notcontains $_.Fu
 $CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $CurrentUserIsAdmin = Test-IsAdmin
 
-# function prompt
-# {
-#     $host.ui.rawui.WindowTitle = "Files: " + (get-childitem).count + " Process: " + (get-process).count
-#     if ($CurrentUserIsAdmin) {
-#         Write-Host ("$($CurrentUser.Name) " + $(get-location) +">") -nonewline -foregroundcolor Red 
-#     } else {
-#         Write-Host ("$($CurrentUser.Name) " + $(get-location) +">") -nonewline -foregroundcolor Green 
-#     }
-#     return " "
-# }
-
-Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
-
-Import-Module "$dotfiles\git\PowershellModules\posh-git"
-
-function global:prompt {
-    $realLASTEXITCODE = $LASTEXITCODE
-
-    # Reset color, which can be messed up by Enable-GitColors
-    $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
-
-    Write-Host($pwd.ProviderPath) -nonewline
-
-    Write-VcsStatus
-
-    $global:LASTEXITCODE = $realLASTEXITCODE
-    
+function global:prompt {    
     if ($CurrentUserIsAdmin) {
-        return " #> "
+        return "$(Get-Location) #> "
     } else {
-        return " $> "
+        return "$(Get-Location) $> "
     }
 }
-
-Enable-GitColors
-
-Pop-Location
-
-Start-SshAgent -Quiet
