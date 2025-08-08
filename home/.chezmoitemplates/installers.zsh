40e5bd3167fbd426491be6b6135b1a3e9bf2d1fb
Paul Shirley <paul@paulshir.com>
2025-08-08 23:09:30 +0100

    feat: Improve (and simplify) package installers

--- home/.chezmoitemplates/installers.zsh
+++ home/.chezmoitemplates/installers.zsh
@@ -1,54 +1,50 @@
-{{- /* Usage:
-...
-*/ -}}
-{{- if not (hasKey . "ctx") -}}{{ fail "must past context ton installers.zsh under ctx key" }}{{- end -}}
+{{- $ctx := first . -}}
+{{- $installer := index . 1 -}}
+{{- $installerArgs := index . 2 -}}
+{{- $packages := index . 3 -}}
 
-{{- if and (hasKey . "brew") .brew -}}{{ "" }}
-brew bundle --no-lock --file=/dev/stdin <<EOF
-{{ range .brew -}}
+{{- if not $packages -}}{{ "" }}
+# No-op, no packages
+
+{{- else if eq $installer "system" -}}{{ "" }}
+# No-op for packages {{ $packages | join ", " }}
+
+{{- else if eq $installer "brew" -}}{{ "" }}
+brew bundle --file=/dev/stdin <<EOF
+{{ range $packages -}}
 brew {{ . | quote }}
 {{ end -}}
 EOF
-{{ end -}}
 
-{{- if and (hasKey . "brewCask") .brewCask -}}{{ "" }}
-echo brew bundle --no-lock --file=/dev/stdin <<EOF
-{{ range .brewCask -}}
+
+{{- else if eq $installer "brewCask" -}}{{ "" }}
+brew bundle --file=/dev/stdin <<EOF
+{{ range $packages -}}
 cask {{ . | quote }}
 {{ end -}}
 EOF
-{{ end -}}
 
-{{- if and (hasKey . "yum") .yum -}}{{ "" }}
-sudo yum install -y {{ range .yum }}{{ . }} {{ end }}
-{{ end -}}
 
-{{- if and (hasKey . "cargo") .cargo -}}{{ "" }}
-{{- $has := includeTemplate "has" (list .ctx "rustToolchainStable" "rustToolchainNightly") | fromToml -}}
-{{- if not (or $has.rustToolchainStable $has.rustToolchainNightly) -}}{{- fail "Cargo installs require rust toolchain" -}}{{- end -}}
+{{- else if eq $installer "yum" -}}{{ "" }}
+sudo yum install -y {{ range $packages }}{{ . }} {{ end }}
+
+
+{{- else if eq $installer "cargo" -}}{{ "" }}
 source ~/.cargo/env
-{{ range .cargo -}}
+{{ range $packages -}}
 cargo install --locked {{ . | quote }}
 {{ end -}}
+
+
+{{- else if eq $installer "mise" -}}{{ "" }}
+{{ range $packages -}}
+mise install {{ . | quote }}
 {{ end -}}
 
-{{- if and (hasKey . "custom") .custom -}}{{ "" }}
-{{ range .custom -}}
+{{- else if hasPrefix "custom" $installer -}}{{ "" }}
+{{ $script := trimPrefix "custom:" $installer }}
+echo {{ $ctx.chezmoi.sourceDir }}/scripts/installers/{{ $script }} {{ $packages | join " " }}
 
-{{- if eq . "helixEditor" -}}
-{{- $helixVersion := (gitHubLatestRelease "helix-editor/helix").TagName -}}{{ "" }} 
-(cd ~/.local/share
-if [[ ! -d helix ]]; then
-	git clone https://github.com/helix-editor/helix
-	(cd helix
-	git reset --hard {{ $helixVersion }}
-	cargo install --path helix-term --locked
-	mkdir -p ~/.config/helix
-	ln -Ts $PWD/runtime ~/.config/helix/runtime
-	)
-fi)
-{{ end -}}
-
-
-{{ end -}}
-{{ end -}}{{- /* End of Custom Installers */ -}}
+{{- else -}}
+{{- print "No installer found for " $installer | fail -}}
+{{- end -}}
